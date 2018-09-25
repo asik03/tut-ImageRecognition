6c72801a16211bca3c7fe836cd75c875187f5d63
Isma <ismaperuga@gmail.com>
2018-09-25 15:47:21 +0300

    Sin dropout, cambiando learning rate

--- src/cnn/cnn_smile.py
+++ src/cnn/cnn_smile.py
@@ -9,61 +9,63 @@ class CNNSmile:
     def __init__(self, dropout_prob=0.25):
         self.dropout_prob = dropout_prob
         self.model = None
-    
+
     def build_model(self, input_shape):
         model = Sequential()
-        
+
         model.add(Conv2D(32, (3, 3), padding='same', input_shape=input_shape))
         model.add(Activation('relu'))
-        
+
         model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(self.dropout_prob))
-        
+        # model.add(Dropout(self.dropout_prob))
+
         model.add(Conv2D(32, (3, 3), padding='same'))
         model.add(Activation('relu'))
-        
+
         model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(self.dropout_prob))
-        
+        # model.add(Dropout(self.dropout_prob))
+
         model.add(Conv2D(32, (3, 3), padding='same'))
         model.add(Activation('relu'))
-        
+
         model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(self.dropout_prob))
-        
+        # model.add(Dropout(self.dropout_prob))
+
         model.add(Flatten())
-        
+
         model.add(Dense(128))
         model.add(Activation('relu'))
-        model.add(Dropout(self.dropout_prob))
-        
+        # model.add(Dropout(self.dropout_prob))
+
         model.add(Dense(1))
+
         model.add(Activation('sigmoid'))
-        
-        optimizer = keras.optimizers.Adam()
-        
+
+        optimizer = keras.optimizers.adam(lr=0.005)
+
         model.compile(loss='binary_crossentropy',
                       optimizer=optimizer,
                       metrics=['accuracy'])
         self.model = model
-    
-    def train_model(self, x_train, y_train, 
-                    batch_size=32, epochs=50):        
+        # model.summary()
+
+    def train_model(self, x_train, y_train,
+                    batch_size=32, epochs=50):
         self.model.fit(x_train, y_train,
                        batch_size=batch_size,
                        epochs=epochs,
                        shuffle=True)
-        
+
     def train_generator_model(self, train_generator,
                               epochs=50):
         self.model.fit_generator(train_generator,
                                  epochs=epochs)
-    
+
     def evaluate_model(self, x_test, y_test):
-        scores = self.model.evaluate(x_test, y_test, 
+        scores = self.model.evaluate(x_test, y_test,
                                      verbose=1)
         print('Test loss:', scores[0])
         print('Test accuracy:', scores[1])
-    
+
     def save_model(self, model_path):
         self.model.save(model_path)
--- src/processes/train.py
+++ src/processes/train.py
@@ -6,10 +6,10 @@ if __name__ == '__main__':
     batch_size = 32
     input_shape = (64, 64, 3)
 
-    train_generator = DataLoader.get_train_generator('C:\\Users\\Jorge\\PycharmProjects\\image-recognition\\etc\\',
-                                                     ['smile', 'non_smile'],
+    train_generator = DataLoader.get_train_generator('D:\\UNIVERSIDAD\MASTER\\(2) SEGUNDO\\Advanced Signal Processing Laboratory\\IMAGE DETECTION\\image-recognition\\etc',
+                                                     ['non_smile', 'smile'],
                                                      batch_size)
     cnn_smile = CNNSmile()
     cnn_smile.build_model(input_shape)
 
-    cnn_smile.train_generator_model(train_generator, epochs=1)
+    cnn_smile.train_generator_model(train_generator, epochs=20)
